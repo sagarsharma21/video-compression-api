3e00a889e34c5493919cedbbaf70120f73e85a2d
sagarsharma21 <007sharma.sagar@gmail.com>
2023-11-11 10:17:36 +0530

    Create video compression api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const fileupload = require("express-fileupload");
 
 //Create an express application instance
 const PORT = 5000;
@@ -10,9 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Add file upload middleware
+app.use(
+    fileupload({
+        tempFIleDir: "temp",
+        useTempFiles: true,
+    })
+);
+
 //Routes
 app.get("/", (req,res) => {
     res.send("Hello world!")
+});
+
+//Create a Post route
+app.post("/compress-video", (req, res) => {
+    console.log(req.files.video);
+    res.send("Success");
 });
 
 //Start the server
